92b0d6421f2e441e35ceeebf1accbb348ae9d9fc
Michael Rooney <msroone@gmail.com>
2026-09-16 19:19:05 -0500

    playing with UI-hammers

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,32 @@ function App() {
 
   return (
 
-    <div>
+    <div className="p-4">
           <h1 className="text-3xl font-bold">Get started</h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
         <button
           type="button"
-          className="btn btn-primary btn-lg btn-soft"
+          className="btn btn-primary btn-lg btn-soft w-full lg:w-auto"
           onClick={() => setCount((count) => count + 1)}
         >
           Count is {count}
         </button>
+
+<div className="card bg-base-100 w-full shadow-sm">
+  <div className="card-body">
+    <h2 className="card-title">Card title!</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+
+
         </div>
+        
   )
 }
 
