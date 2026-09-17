3fb1ce928775e77962e14b83299c0a3ace2a95d5
Michael Rooney <msroone@gmail.com>
2026-09-17 18:50:09 -0500

    UI-hammers

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
 import './App.css'
-import { Header } from './components/header'
+import { NavBar } from './components/NavBar.tsx'
 
-function App() {
+const  App = () => {
   const [count, setCount] = useState(0)
 
   return (
-
-<div className="App" data-theme="luxury">
-      <Header />
-      <div className="p-4">
+    <div className="p-4">
+      <NavBar />
+    
           <h1 className="text-3xl font-bold">Get started</h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
@@ -21,27 +20,28 @@ function App() {
         >
           Count is {count}
         </button>
-      <div className="hover-3d">
-      <div className="card bg-base-100 w-full shadow-sm">
-        <div className="card-body">
-          <h2 className="card-title">Card title!</h2>
-          <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
-          <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      </div>
-      </div>
-      </div>
+<div className="hover-3d">
+<div className="card bg-base-100 w-full shadow-sm">
+  <div className="card-body">
+    
+    <h2 className="card-title">Card title!</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+    <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
 </div>
+</div>
+</div>
+
+        </div>
         
   )
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,4 @@
+import '../App.css'
+
+export const NavBar = () => (<input type="checkbox" value="synthwave" className="toggle theme-controller" />
+)
