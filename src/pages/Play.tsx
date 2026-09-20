c1cf2724ebdfd47b8b28a1cad698c448785f96ad
Michael Rooney <msroone@gmail.com>
2026-09-20 16:40:37 -0500

    Debug Session playing with DaisyUI

--- src/pages/Play.tsx
+++ src/pages/Play.tsx
@@ -7,7 +7,7 @@ export const Play = () => {
             <h1>Play</h1>
             <button 
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                onClick={() => nav('/')}
+                onClick={() => nav('-1')}
             >
                 Go Home
             </button>
