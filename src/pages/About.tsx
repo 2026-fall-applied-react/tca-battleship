f309442c5d7e1f52b212d1b5aedb084ee05a478d
Michael Rooney <msroone@gmail.com>
2026-09-22 14:35:48 -0500

    Navigation alterations

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -7,7 +7,7 @@ export const About = () => {
             <h1>About</h1>
             <button 
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                onClick={() => nav(-1)}
+                onClick={() => nav('/play')}
             >
                 Go Home
             </button>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,17 +3,19 @@ import { NavBar } from "../components/NavBar.tsx";
 
 export const Home = () => {
     const nav = useNavigate();
+
     return (
-        <div>
-
-            <h1>Home</h1>
-            <button 
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                onClick={() => nav('/about')}
-            >
-                Click me
-            </button>
-        </div>
-    )
-
+        <>
+            <NavBar />
+            <div>
+                <h1>Home</h1>
+                <button
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    onClick={() => nav('/about')}
+                >
+                    Click me
+                </button>
+            </div>
+        </>
+    );
 };
