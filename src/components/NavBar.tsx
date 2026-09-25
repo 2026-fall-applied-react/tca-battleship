d2d718e1128f24f882a7c0132f18abf298720e89
Michael Rooney <msroone@gmail.com>
2026-09-25 15:04:43 -0500

    navbar is coming together, use of { Link } from 'react-router';

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router';
 import '../App.css';
 
 interface NavBarProps {
@@ -6,6 +7,13 @@ interface NavBarProps {
 }
 
 export const NavBar = ({ theme, onToggleTheme }: NavBarProps) => (
+  <div className="flex justify-between items-center p-2">
+    <nav className="flex gap-4">
+      <Link to="/">Home</Link>
+      <Link to="/setup">Setup</Link>
+      <Link to="/play">Play</Link>
+      <Link to="/about">About</Link>
+    </nav>
   <div className="flex justify-end">
     <label className="swap swap-rotate">
   <input
@@ -33,5 +41,6 @@ export const NavBar = ({ theme, onToggleTheme }: NavBarProps) => (
       d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
   </svg>
 </label>
+  </div>
   </div>
 );
